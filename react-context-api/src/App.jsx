215f725e4692a76fa634a75c61dde3b9b80f8a50
Steopa2001 <moraru495@gmail.com>
2025-09-09 20:21:15 +0200

    feat(router): add routes for products list and product detail

--- react-context-api/src/App.jsx
+++ react-context-api/src/App.jsx
@@ -1,6 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Products from "./pages/Products.jsx";
+import Product from "./pages/Product.jsx";
 
-function App() {
-  
-}
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <main className="container py-4">
+        <Routes>
+          <Route path="/" element={<Products />} />
+          <Route path="/product/:id" element={<Product />} />
+        </Routes>
+      </main>
+    </>
+  );
+};
 
 export default App;
